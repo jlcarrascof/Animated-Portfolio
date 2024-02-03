995c9d3b4be116f1d8003e7fc04aab9f853a0dbd
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-02-02 21:00:49 -0400

    Testing framer-motion - Part 2

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,16 +1,25 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const Test = () => {
+
+  const [ open, setOpen ] = useState(false)
+
+  const variants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, x: 600, transition: { type: 'spring', stiffness: 100, damping: 100}}  
+  }  
+
   return (
     <div className="course">
         <motion.div className="box"
-            initial={{opacity: 0.5, scale: 0.5}}
-            transition={{duration: 2}}
-            whileInView={{opacity: 1, scale: 2}}
-            drag
+            variants={variants}
+            // initial="hidden"
+            // animate="visible"
+            animate={open ? "visible" : "hidden"}   
         >
-
         </motion.div>
+        <button onClick={() => setOpen(prev=>!prev)}>Click</button>
     </div>
   )  
 }
